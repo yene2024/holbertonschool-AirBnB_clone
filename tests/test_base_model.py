192257f43b859765562d93bb69e6359160f9f277
yene2024 <yeneishla1524@gmail.com>
2024-02-23 17:10:40 -0400

    fixing tests files.

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,32 +1,15 @@
 #!/usr/bin/python3
-import unittest
 from models.base_model import BaseModel
 
-
-class TestBaseModel(unittest.TestCase):
-    def test_init(self):
-        bm = BaseModel()
-        self.assertIsInstance(bm, BaseModel)
-        self.assertTrue(hasattr(bm, 'id'))
-        self.assertTrue(hasattr(bm, 'created_at'))
-        self.assertTrue(hasattr(bm, 'updated_at'))
-
-    def test_str(self):
-        bm = BaseModel()
-        self.assertIsInstance(str(bm), str)
-
-    def test_save(self):
-        bm = BaseModel()
-        bm.save()
-        self.assertNotEqual(bm.created_at, bm.updated_at)
-
-    def test_to_dict(self):
-        bm = BaseModel()
-        obj_dict = bm.to_dict()
-        self.assertIsInstance(obj_dict, dict)
-        self.assertEqual(obj_dict['__class__'], 'BaseModel')
-
-    def test_to_json_string(self):
-        bm = BaseModel()
-        json_string = bm.to_json_string()
-        self.assertIsInstance(json_string, str)
+my_model = BaseModel()
+my_model.name = "My First Model"
+my_model.my_number = 89
+print(my_model)
+my_model.save()
+print(my_model)
+my_model_json = my_model.to_dict()
+print(my_model_json)
+print("JSON of my_model:")
+for key in my_model_json.keys():
+    print("\t{}: ({}) - {}".format(
+        key, type(my_model_json[key]), my_model_json[key]))
