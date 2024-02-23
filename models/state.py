e5d04b2650f76abf6855ecd96b32ab21a50704e0
yene2024 <yeneishla1524@gmail.com>
2024-02-23 15:32:32 -0400

    State class for representing a state.

---
+++ models/state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+
+class State(BaseModel):
+    """
+    State class for representing a state.
+
+    Attributes:
+        name (str): The name of the state.
+    """
+    def __init__(self, *args, **kwargs):
+        """Initializes a new State instance."""
+        super().__init__(*args, **kwargs)
+        self.name = ""
