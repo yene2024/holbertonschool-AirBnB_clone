10d313ab129b3107a42f18def57eda68742e0e93
yene2024 <yeneishla1524@gmail.com>
2024-02-23 15:41:37 -0400

    Fixing pycodestyle

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,6 +6,7 @@ import json
 from models.base_model import BaseModel
 from models.user import User
 
+
 class FileStorage:
     __file_path = "file.json"
     __objects = {}
--- models/review.py
+++ models/review.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from models.base_model import BaseModel
 
+
 class Review(BaseModel):
     """
     Review class for representing a review.
