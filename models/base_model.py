90a7a65c5ba72084fc35e8cbc51cf4d8baa8d533
yene2024 <yeneishla1524@gmail.com>
2024-02-25 18:49:37 -0400

    Fixing the file.

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ Module containing the BaseModel class
 """
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -26,6 +27,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         return "[{}] ({}) {}".format(
