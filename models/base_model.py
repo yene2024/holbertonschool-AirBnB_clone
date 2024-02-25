ffeb423512a510f89eaef4bd3ce35ca8f8a7981b
yene2024 <yeneishla1524@gmail.com>
2024-02-25 18:43:56 -0400

    Fixing the code.

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,6 @@ Module containing the BaseModel class
 """
 import uuid
 from datetime import datetime
-from models import storage
 
 
 class BaseModel:
@@ -27,19 +26,13 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
 
     def __str__(self):
         return "[{}] ({}) {}".format(
             self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """
-        Updates the public instance attribute
-        updated_at with the current datetime.
-        """
         self.updated_at = datetime.now()
-        storage.save()
 
     def to_dict(self):
         model_dict = self.__dict__.copy()
@@ -47,7 +40,3 @@ class BaseModel:
         model_dict['created_at'] = self.created_at.isoformat()
         model_dict['updated_at'] = self.updated_at.isoformat()
         return model_dict
-
-
-if __name__ == '__main__':
-    my_model = BaseModel()
