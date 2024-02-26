8fd242c3fbbed61857433805272a63f78c32c417
yene2024 <yeneishla1524@gmail.com>
2024-02-26 11:16:04 -0400

    Change the code.

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,6 @@ Module containing the BaseModel class
 """
 import uuid
 from datetime import datetime
-from models import storage
 
 
 class BaseModel:
@@ -27,7 +26,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
 
     def __str__(self):
         return "[{}] ({}) {}".format(
@@ -35,7 +33,6 @@ class BaseModel:
 
     def save(self):
         self.updated_at = datetime.now()
-        storage.save()
 
     def to_dict(self):
         model_dict = self.__dict__.copy()
@@ -43,4 +40,3 @@ class BaseModel:
         model_dict['created_at'] = self.created_at.isoformat()
         model_dict['updated_at'] = self.updated_at.isoformat()
         return model_dict
-
