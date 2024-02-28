a8d09aa106fd542ae3438874ca80f30f91e15cfb
Angelo Garcia <angelogarcia1029@gmail.com>
2024-02-28 17:54:51 -0400

    Unittest

--- models/base_model.py
+++ models/base_model.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
+"""BaseModel module"""
 import uuid
 from datetime import datetime
 import models
 
 
 class BaseModel():
+    """BaseModel class"""
+
     def __init__(self, *args, **kwargs):
+        """Initializes an instance of BaseModel.
+        """
         self.is_new_instance = True
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
@@ -24,13 +29,21 @@ class BaseModel():
         models.storage.new(self)
 
     def __str__(self):
+        """Return the string representation of the BaseModel instance.
+        """
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
+        """Updates the public instance attribute updated_at
+        with the current datetime.
+        """
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
+        """Returns a dictionary containing all keys/values of
+        __dict__ of the instance.
+        """
         model_dict = self.__dict__.copy()
         model_dict['__class__'] = self.__class__.__name__
         model_dict['created_at'] = self.created_at.isoformat()
