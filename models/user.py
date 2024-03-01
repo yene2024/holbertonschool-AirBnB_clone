386bd02695334b489d4a2fa07eb91b7e9cfda826
Angelo Garcia <angelogarcia1029@gmail.com>
2024-03-01 11:49:52 -0400

    corrected public instance variables for User

--- models/user.py
+++ models/user.py
@@ -7,6 +7,10 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """User class that inherits from BaseModel."""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
 
     def __init__(self, *args, **kwargs):
         """Initialize User instance."""
