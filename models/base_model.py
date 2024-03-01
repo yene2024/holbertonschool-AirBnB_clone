dc2b670ff0f67f862f41ca80c949595e8e6e2c51
Angelo Garcia <angelogarcia1029@gmail.com>
2024-03-01 13:16:27 -0400

    modifying class declaration

--- models/base_model.py
+++ models/base_model.py
@@ -16,26 +16,25 @@ class BaseModel:
         """
         Initializes a new instance of BaseModel.
         """
-        time_Format = "%Y-%m-%dT%H:%M:%S.%f"
+        timeFormat = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
         if kwargs:
-            for key, value in kwargs.items():
+            for key, val in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
-                    setattr(self, key, datetime.strptime(
-                        value, time_Format))
+                    self.__dict__[key] = datetime.strptime(val, timeFormat)
                 else:
-                    setattr(self, key, value)
-            self.id = kwargs.get('id', str(uuid.uuid4()))
+                    self.__dict__[key] = val
         else:
             models.storage.new(self)
 
     def __str__(self):
         """Returns a string representation of the instance.
         """
+        class_name = self.__class__.__name__
         return "[{}] ({}) {}".format(
-            self.__class__.__name__, self.id, self.__dict__)
+            class_name, self.id, self.__dict__)
 
     def save(self):
         """Updates the public instance attribute updated_at"""
