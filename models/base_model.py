8289056562f180f2f46113c05f7ff264e30bac93
Angelo Garcia <angelogarcia1029@gmail.com>
2024-03-01 12:59:53 -0400

    Fixing attribute declarations

--- models/base_model.py
+++ models/base_model.py
@@ -16,18 +16,19 @@ class BaseModel:
         """
         Initializes a new instance of BaseModel.
         """
+        time_Format = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
         if kwargs:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
                     setattr(self, key, datetime.strptime(
-                        value, "%Y-%m-%dT%H:%M:%S.%f"))
-                elif key != '__class__':
+                        value, time_Format))
+                else:
                     setattr(self, key, value)
             self.id = kwargs.get('id', str(uuid.uuid4()))
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
             models.storage.new(self)
 
     def __str__(self):
